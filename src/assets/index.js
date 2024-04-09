94f94ae9c281f7571eac95ef9eed2cba15b1ce0d
anuragnegi000 <anurag.negi862@gmail.com>
2024-04-09 14:04:33 +0530

    latest

--- src/assets/index.js
+++ src/assets/index.js
@@ -49,10 +49,10 @@ import stars from "./pricing/stars.svg";
 import coins from "./roadmap/coins.png";
 import done from "./roadmap/done.svg";
 import hero from "./roadmap/hero.png";
-import roadmap1 from "./roadmap/image-1.png";
-import roadmap2 from "./roadmap/image-2.png";
-import roadmap3 from "./roadmap/image-3.png";
-import roadmap4 from "./roadmap/image-4.png";
+import roadmap1 from "./roadmap/soul_Logo.png";
+import roadmap2 from "./roadmap/keepers.jpg";
+import roadmap3 from "./roadmap/soul_Sid.png";
+import roadmap4 from "./roadmap/clash.jpg";
 import undone from "./roadmap/undone.svg";
 
 import notification1 from "./notification/pfp3.jpg";
