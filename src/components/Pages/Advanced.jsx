9a8d5d1763cf5a8dfbc33965749a28457b9ea3dd
GUNGODD <chauhanadityac01@gmail.com>
2024-04-17 14:57:58 -0700

    iframe error

--- src/components/Pages/Advanced.jsx
+++ src/components/Pages/Advanced.jsx
@@ -6,6 +6,36 @@ import Header from "../Header";
 import Footer from "../Footer";
 import Slider from 'react-slick';
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: "linear",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1
+      }
+    }
+  ]
+};
+
 function ServicePage() {
   const [portfolioExamples, setPortfolioExamples] = useState([]);
 
@@ -44,9 +74,9 @@ function ServicePage() {
             <h2 className="text-xl font-semibold">Portfolio Examples</h2>
             <Slider {...settings}>
               {portfolioExamples.map(example => (
-                <div key={example.id} className="p-2">
+                <div key={example.id} className="p-2 image-container">
                   <img src={example.imgSrc} alt={example.description} style={{ width: '100%', height: 'auto', aspectRatio: '16/9', borderRadius: '8px' }} />
-                  <p className="image-description">{example.description}</p>
+                  <p>{example.description}</p>
                 </div>
               ))}
             </Slider>
