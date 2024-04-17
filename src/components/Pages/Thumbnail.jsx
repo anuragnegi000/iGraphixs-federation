191977ee04609539b32b026c6c52eca72836007f
anuragnegi000 <anurag.negi862@gmail.com>
2024-04-17 19:48:14 +0530

    fixes

--- src/components/Pages/Thumbnail.jsx
+++ src/components/Pages/Thumbnail.jsx
@@ -1,23 +1,6 @@
 import ButtonGradient from "../../assets/svg/ButtonGradient"
 import Footer from "../Footer"
 import Header from "../Header"
-
-<<<<<<< HEAD:src/components/Pages/MinPages.jsx
-import ButtonGradient from "../../assets/svg/ButtonGradient";
-import Footer from "../Footer";
-import Header from "../Header";
-
-export const MinPages = () => {
-  return <></>;
-};
-
-export default function MainPage() {
-  return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-<div className="mt-5 p-4 ">
-=======
 export const Thumbnail = () => {
   return (
    <>
@@ -37,15 +20,6 @@ export const Thumbnail = () => {
    </>
   )
 }
->>>>>>> d7ccb5f335bbdfe7cfda702910a2658dcd25310d:src/components/Pages/Thumbnail.jsx
 
-  Hello i am Pager 👻
-</div>
-        <Footer />
-      </div>
-      <ButtonGradient />
-    </>
-  );
-}
 
 export default Thumbnail;
