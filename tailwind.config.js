4c3e352427a62d049712ab33d07a6ca38637cf30
GUNGODD <chauhanadityac01@gmail.com>
2024-04-26 04:44:44 +0530

    feature: modal added  #5

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        contentShow: {
+          from: { opacity: '0', transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+
       colors: {
         color: {
           1: "#AC6AFF",
