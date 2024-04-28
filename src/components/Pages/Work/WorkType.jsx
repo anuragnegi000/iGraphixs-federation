253a7676c71a3a30c0edccf3794883517f4bc648
GUNGODD <chauhanadityac01@gmail.com>
2024-04-28 21:15:14 +0530

    Card : WorkType

---
+++ src/components/Pages/Work/WorkType.jsx
@@ -0,0 +1,37 @@
+import Header from "../../Header";
+
+import Footer from "../../Footer";
+export const WorkType = () => {
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+    },
+
+    {
+      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+    },
+  ];
+  return (
+    <>
+      <div className=" z-10 m-5 !px-0 !py-10  md-9 pt-16 ">
+        <Header />
+      </div>
+
+      <div>
+        <h2 className="!px-0 !py-10 text-center"> hello</h2>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default WorkType;
