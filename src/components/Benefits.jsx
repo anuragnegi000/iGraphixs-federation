5fc6ad7084af983f08909c12b42f84e656b67fc5
GUNGODD <chauhanadityac01@gmail.com>
2024-04-28 12:35:54 +0530

    pop over

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,4 +1,4 @@
-import { Button, Modal } from "flowbite-react";
+import { Button, Modal, Popover } from "flowbite-react";
 import { useState } from "react";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 
@@ -11,6 +11,19 @@ import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   const [openModal, setOpenModal] = useState(true);
+
+  const content = (
+    <div className="w-64 text-sm text-gray-500 dark:text-gray-400">
+      <div className="border-b border-gray-200 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-gray-700">
+        <h3 className="font-semibold text-gray-900 dark:text-white">
+          Popover title
+        </h3>
+      </div>
+      <div className="px-3 py-2">
+        <p>And here's some amazing content. It's very engaging. Right?</p>
+      </div>
+    </div>
+  );
 
   return (
     <Section id="features">
@@ -37,40 +50,10 @@ const Benefits = () => {
                       alt={item.title}
                     />
                     <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                      <Button onClick={() => setOpenModal(true)} type="modal">
+                      {/* <Button onClick={() => setOpenModal(false)} type="modal">
                         {" "}
                         Test{" "}
-                      </Button>
-                      <Modal
-                        show={openModal}
-                        size="md"
-                        onClose={() => setOpenModal(false)}
-                        popup
-                      >
-                        <Modal.Header />
-                        <Modal.Body>
-                          <div className="text-center">
-                            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                              Are you sure you want to delete this product?
-                            </h3>
-                            <div className="flex justify-center gap-4">
-                              <Button
-                                color="failure"
-                                onClick={() => setOpenModal(false)}
-                              >
-                                {"Yes, I'm sure"}
-                              </Button>
-                              <Button
-                                color="gray"
-                                onClick={() => setOpenModal(false)}
-                              >
-                                No, cancel
-                              </Button>
-                            </div>
-                          </div>
-                        </Modal.Body>
-                      </Modal>
+                      </Button> */}
                     </p>
                     <Arrow />
                   </div>
@@ -98,6 +81,33 @@ const Benefits = () => {
           ))}
         </div>
       </div>
+      <Modal
+        show={openModal}
+        size="md"
+        onClose={() => setOpenModal(false)}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Visit Our Portfolio 😊
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="pink" onClick={() => setOpenModal(false)}>
+                {"Gallery 1"}
+              </Button>
+              <Button color="green" onClick={() => setOpenModal(false)}>
+                {"Gallery 2"}
+              </Button>
+              <Button color="blue" onClick={() => setOpenModal(false)}>
+                {"Gallery 3"}
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </Section>
   );
 };
