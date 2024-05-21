e1dab628ca8c26f80d1f4582c2167cc2b15079ce
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-21 20:47:04 +0530

    changes in pricing section

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -5,71 +5,20 @@ import Button from "./Button";
 
 const PricingList = () => {
 
-  const [data,setData]=useState(2);
+  const [data, setData] = useState(2);
 
-  const amount = (data)*100;
+  const amount = (data) * 100;
   const currency = "INR";
   const receiptId = "Receipt no. 1";
 
-  const paymentHandler = async (e) => {
-    
-      const response = await fetch("http://localhost:5000/order", {
-        method: "POST",
-        body: JSON.stringify({
-          amount,
-          currency,
-          receipt: receiptId,
-        }),
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      const order = await response.json();
-      console.log(order);
-
-      var options = {
-        "key": "rzp_test_CvTTtX7qEECnrc", // Enter the Key ID generated from the Dashboard
-        amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-        currency,
-        "name": "Acme Corp", //your business name
-        "description": "Test Transaction",
-        "image": "https://example.com/your_logo",
-        "order_id":order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-        "handler": function (response){
-            alert(response.razorpay_payment_id);
-            alert(response.razorpay_order_id);
-            alert(response.razorpay_signature)
-        },
-        "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
-            "name": "Anurag negi", //your customer's name
-            "email": "anurag.negi862@gmail.com", 
-            "contact": "9123456789"  //Provide the customer's phone number for better conversion rates 
-        },
-        "notes": {
-            "address": "Razorpay Corporate Office"
-        },
-        "theme": {
-            "color": "#3399cc"
-        }
-    };
-    var rzp1 = new window.Razorpay(options);
-    rzp1.on('payment.failed', function (response){
-            alert(response.error.code);
-            alert(response.error.description);
-            alert(response.error.source);
-            alert(response.error.step);
-            alert(response.error.reason);
-            alert(response.error.metadata.order_id);
-            alert(response.error.metadata.payment_id);
-    });
-    rzp1.open();
-    e.preventDefault();
-   
+  const paymentHandler = () => {
+    const whatsappLink = "https://wa.me/message/ZPEYAJ2WUS26C1";
+    window.open(whatsappLink, 'Hey i want to buy');
   };
 
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
-      {pricing.map((item, index) => ( // Added unique key prop
+      {pricing.map((item, index) => (
         <div
           key={index}
           className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
@@ -81,24 +30,17 @@ const PricingList = () => {
           </p>
 
           <div className="flex items-center h-[5.5rem] mb-6">
-            
-              
-              <>
-              
-                <div className="h3">$</div>
-                <div className="text-[5.5rem] leading-none font-bold">
-                  {item.price}
-                  
-                  
-                </div>
-              </>
-            
+            <>
+              <div className="h3">$</div>
+              <div className="text-[5.5rem] leading-none font-bold">
+                {item.price}
+              </div>
+            </>
           </div>
 
           <Button
             onClick={paymentHandler}
             className="w-full mb-6"
-            // href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
             white={!!item.price}
           >
             {item.price ? "Press to continue" : "Contact us"}
