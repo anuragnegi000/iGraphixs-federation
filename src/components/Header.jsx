bf9cbf90b3d2112acd25959e0909bb7ace3700f4
Anurag Negi <115611556+anuragnegi000@users.noreply.github.com>
2024-06-11 19:21:56 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm" // added navigation
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
